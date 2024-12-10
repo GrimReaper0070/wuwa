989d3dbd4a913db7a0e9fc08ec181635489830ff
GrimReaper0070 <faketool2018@gmail.com>
2024-12-10 16:38:28 +0600

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+    <footer className="w-screen bg-[#f4f5fa] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           ©Ashraful Talukder 2024. All rights reserved
